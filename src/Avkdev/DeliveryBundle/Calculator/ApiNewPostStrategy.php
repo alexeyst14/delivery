5749546b3009ee5ee04ff09d37065e394ac8f396
Alexey Kiselev <a.kiselev@petrosoftinc.com>
2014-09-11 19:10:28 +0300

    Development stage

--- src/Avkdev/DeliveryBundle/Calculator/ApiNewPostStrategy.php
+++ src/Avkdev/DeliveryBundle/Calculator/ApiNewPostStrategy.php
@@ -10,18 +10,22 @@ namespace Avkdev\DeliveryBundle\Calculator;
 
 
 use Symfony\Component\DependencyInjection\ContainerAware;
+use Symfony\Component\HttpFoundation\Request;
 
 class ApiNewPostStrategy extends ContainerAware implements CalculatorStrategyInterface
 {
     public function calculate(array $data)
     {
-        sleep(10);
+        /** @var $request Request */
+        $request = $this->container->get('request');
+
+
+        sleep(2);
         return array (
-            array(
-                'company' => 'Новая Почта',
-                'delivery_time' => '3',
-                'cost' => '44.55',
-            )
+            'company_id' => '121',
+            'company' => 'Новая Почта',
+            'delivery_time' => '3',
+            'cost' => 43.55 + mt_rand(1, 10),
         );
     }
 }
